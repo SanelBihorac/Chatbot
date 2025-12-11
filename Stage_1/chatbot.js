eaa8c85298402bdd95b5709b2e50fff990fd94f2
SanelBihorac <sbihorac85@gmail.com>
2025-12-11 10:51:33 +0100

    Second Commit

--- Stage_1/chatbot.js
+++ Stage_1/chatbot.js
@@ -2,3 +2,4 @@
 
 console.log("Hello! My name is Aid.")
 console.log('I was created in 2023.')
+
---
+++ Stage_2/chatbot.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+
+console.log("Hello! My name is Aid.");
+console.log("I was created in 2025.");
+let name = prompt("What is your name? ")
+console.log(`What a great name you have, ${name}!`)
+console.log("What a great name you have, " + name + "!")
+
+
